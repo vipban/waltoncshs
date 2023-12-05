410327e4fcad4b0958bd8b16959799d86f81823f
vipban <145408790+vipban@users.noreply.github.com>
2023-12-04 23:03:04 -0500

    Finish basic homepage materials slideshow

---
+++ src/components/slideshow.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useState } from 'react'
+import { ArrowBigLeft, ArrowBigRight } from 'lucide-react'
+import './stylesheets/slideshow.css'
+
+export function Slideshow({ imageUrls }) {
+    const [imageIndex, setImageIndex] = useState(0)
+
+    function showNextImage() {
+        setImageIndex(index => {
+            if (index === imageUrls.length - 1) return 0
+            return index + 1
+        })
+    }
+
+    function showPrevImage() {
+        setImageIndex(index => {
+            if (index === 0) return imageUrls.length - 1
+            return index - 1
+        })
+    }
+
+    return (
+        <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+            <section className='slideshow-content'>
+                <img src={imageUrls[imageIndex]} className='img-slider-img'/>
+                <button onClick={showPrevImage} className='img-slider-btn' style={{left: 0}}>
+                    <ArrowBigLeft />
+                </button>
+                <button onClick={showNextImage} className='img-slider-btn' style={{right: 0}}>
+                    <ArrowBigRight />
+                </button>
+            </section>
+        </div>
+    )
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,19 @@
+import car1 from '../resources/blue-maserati.jpg'
+import car2 from '../resources/McLaren-720S.jpg'
+import car3 from '../resources/Audi-R8.jpg'
+
+import { Slideshow } from '../components/slideshow'
+import './stylesheets/home.css'
+
 function Home() {
+    const images = [car1, car2, car3]
+
     return (
         <>
-            <h1>Home</h1>
+            <section className='materials-slideshow'>
+                <h1>Recyclable Materials</h1>
+                <Slideshow imageUrls={images} />
+            </section>
         </>
     )
 }
