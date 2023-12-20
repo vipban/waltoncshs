fb30c1e10b43d83feb923fbbbd571857be95fe2e
vipban <145408790+vipban@users.noreply.github.com>
2023-12-20 00:49:42 -0500

    Add mobile nav menu

--- src/components/header.js
+++ src/components/header.js
@@ -2,10 +2,27 @@ import { Link, useMatch, useResolvedPath } from 'react-router-dom'
 import './stylesheets/header.css'
 
 export default function Header() {
+    function handleClick() {
+        document.getElementById('hamburger-toggle').click()
+    }
+
     return (
         <nav className='nav'>
             <Link to='/' className='site-title'>Walton CSHS</Link>
-            <ul>
+            <div className='mobile-nav'>
+                <label class='hamburger-menu'>
+                    <input type='checkbox' name='' id='hamburger-toggle' />
+                </label>
+                <aside className='sidebar'>
+                    <ul>
+                        <CustomLink to='/' onClick={handleClick}>Home</CustomLink>
+                        <CustomLink to='upcoming-events' onClick={handleClick}>Upcoming Events</CustomLink>
+                        <CustomLink to='sign-ups' onClick={handleClick}>Sign-Ups</CustomLink>
+                        <CustomLink to='officers' onClick={handleClick}>Officers</CustomLink>
+                    </ul>
+                </aside>
+            </div>
+            <ul className='desktop-nav'>
                 <CustomLink to='/'>Home</CustomLink>
                 <CustomLink to='upcoming-events'>Upcoming Events</CustomLink>
                 <CustomLink to='sign-ups'>Sign-Ups</CustomLink>
