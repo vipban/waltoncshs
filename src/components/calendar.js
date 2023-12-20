b08ac1d7a386d6dc533168956fa093042ec47dd7
vipban <145408790+vipban@users.noreply.github.com>
2023-12-20 16:05:55 -0500

    Add event modal and display using mouse xy

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -8,15 +8,26 @@ import { MapPin, CalendarClock, Clock, X } from 'lucide-react'
 import './stylesheets/calendar.css'
 
 export default function CalendarComponent() {
+    const [modalState, setModalState] = useState({ isOpen: false, event: null })
+    const [prevId, setPrevId] = useState('')
+
+    // Event pop-up on event click
     function EventModal({ isOpen, onClose, event }) {
+        const modalClassName = isOpen ? 'event-modal show' : 'event-modal'
+        let top = modalState.top
+        let left = modalState.left
+
         if (isOpen) {
+            // Date/Time Formatting
             let start = new Date(event.start)
             let end = new Date(event.end)
+
             const dateOptions = {
                 month: 'long',
                 day: 'numeric',
                 year: 'numeric'
             }
+
             const timeOptions = {
                 hour: 'numeric',
                 minute: 'numeric'
@@ -26,30 +37,42 @@ export default function CalendarComponent() {
             let startDate = start.toLocaleDateString('en-US', dateOptions)
             let endDate = end.toLocaleDateString('en-US', dateOptions)
 
-            let startTime = start.toLocaleTimeString('en-US', timeOptions);
-            let endTime = end.toLocaleTimeString('en-US', timeOptions);
+            let startTime = start.toLocaleTimeString('en-US', timeOptions)
+            let endTime = end.toLocaleTimeString('en-US', timeOptions)
 
+            // Description Formatting
             let formattedDescription = event.description.slice(0, event.description.indexOf('Slots:'))
         
+            console.log(top, left)
+
             return (
-                <div className="event-modal">
-                    <div className='event-modal-top'>
-                        <h3>{event.title}</h3>
-                        <X onClick={onClose} style={{cursor: 'pointer'}}/>
-                    </div>
-                    <p>{ formattedDescription || 'No description'}</p>
-                    {event.allDay ? <p><CalendarClock /> {startDate} - {endDate}</p> : <p><Clock /> {startDate} ({startTime} - {endTime})</p>}
-                    <p><MapPin /> {event.location}</p>
+                <div className={modalClassName} style={{ top: `${top}px`, left: `${left}px` }}>
+                    <h3 className='flex'>{event.title} <X onClick={onClose} style={{cursor: 'pointer'}}/></h3>
+                    <p className='flex'>{ formattedDescription || 'No description'}</p>
+                    {event.allDay ? <p className='flex'><CalendarClock /> {startDate} - {endDate}</p> : <p><Clock /> {startDate} ({startTime} - {endTime})</p>}
+                    <p className='flex'><MapPin /> {event.extendedProps.location}</p>
                 </div>
-            );
+            )
         }
     }
 
-    const [modalState, setModalState] = useState({ isOpen: false, event: null })
-
     function handleEventClick(info) {
-      info.jsEvent.preventDefault();
-      setModalState({ isOpen: true, event: { ...info.event.toPlainObject(), description: info.event.extendedProps.description } })
+        const { id } = info.event
+
+        if (id !== prevId) {
+            info.jsEvent.preventDefault()
+
+            let { clientX, clientY } = info.jsEvent
+
+            setModalState({
+                isOpen: true,
+                event: { ...info.event.toPlainObject(), description: info.event.extendedProps.description },
+                top: clientY,
+                left: clientX
+            })
+
+           setPrevId(id)
+        }
     }
   
     function closeModal() {
@@ -59,13 +82,15 @@ export default function CalendarComponent() {
     return (
         <>
             <div className='dayGrid'>
+                <EventModal isOpen={modalState.isOpen} onClose={closeModal} event={modalState.event} />
                 <FullCalendar
                     plugins={[dayGridPlugin, googleCalendarPlugin]}
                     initialView='dayGridMonth'
                     googleCalendarApiKey='AIzaSyD_a8nxB1B6ns3qCU5X5RRp-A2u-Gf34qQ'
                     events={{ googleCalendarId: 'dcfcdb0b49805229221411d49688fa71211e21ae00f56ba13d58153b6e554d13@group.calendar.google.com' }}
-                    eventClick={handleEventClick}
-                />    
+                    eventMouseEnter={handleEventClick}
+                    eventMouseLeave={handleEventClick}
+                />
             </div>
             <div className='listView'>
                 <FullCalendar
@@ -75,7 +100,6 @@ export default function CalendarComponent() {
                     events={{ googleCalendarId: 'dcfcdb0b49805229221411d49688fa71211e21ae00f56ba13d58153b6e554d13@group.calendar.google.com' }}
                 />
             </div>
-            <EventModal isOpen={modalState.isOpen} onClose={closeModal} event={modalState.event} />
         </>
     )
 }
