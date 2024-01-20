5013c7e1561dde1bba10f878dfd894a8a7722a0c
vipban <145408790+vipban@users.noreply.github.com>
2024-01-20 17:32:11 -0500

    Add photos without Google Drive

--- src/pages/home.js
+++ src/pages/home.js
@@ -13,24 +13,35 @@ import { collection , getDocs } from 'firebase/firestore'
 function Home() {
     const [photoArray, setPhotoArray] = useState([])
 
+    // useEffect(() => {
+    //     async function fetchData() {
+    //         try {
+    //             const querySnapshot = await getDocs(collection(db, 'slideshow-photos'))
+    //             const photos = querySnapshot.docs.map(doc => ({
+    //                 url: doc.data().url, 
+    //                 alt: doc.data().alt
+    //             }))
+
+    //             setPhotoArray(photos)
+    //             console.log(photos)
+    //         } catch (error) {
+    //             console.error('Error getting documents: ', error)
+    //         }
+    //     }
+
+    //     fetchData()
+    // }, [])
+
     useEffect(() => {
-        async function fetchData() {
-            try {
-                const querySnapshot = await getDocs(collection(db, 'slideshow-photos'))
-                const photos = querySnapshot.docs.map(doc => ({
-                    url: doc.data().url, 
-                    alt: doc.data().alt
-                }))
-
-                setPhotoArray(photos)
-                console.log(photos)
-            } catch (error) {
-                console.error('Error getting documents: ', error)
-            }
+        const importAll = r => {
+            return r.keys().map( filename => ({
+                url: r(fileName).default,
+                alt: fileName.replace('./', '').replace(/\.[^/.]+$/, '')
+            }))
         }
-
-        fetchData()
-    }, [])
+        const photos = importAll(require.context('../resources', false, /\.(png|jpe?g|svg)$/))
+        setPhotoArray(images)
+    })
 
     return (
         <>
