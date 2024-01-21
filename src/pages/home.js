2301f33e737e554cc852e42da77612236f8b8c83
vipban <145408790+vipban@users.noreply.github.com>
2024-01-21 11:45:29 -0500

    Fix errors

--- src/pages/home.js
+++ src/pages/home.js
@@ -13,30 +13,24 @@ import { collection , getDocs } from 'firebase/firestore'
 function Home() {
     const [photoArray, setPhotoArray] = useState([])
 
-    // useEffect(() => {
-    //     async function fetchData() {
-    //         try {
-    //             const querySnapshot = await getDocs(collection(db, 'slideshow-photos'))
-    //             const photos = querySnapshot.docs.map(doc => ({
-    //                 url: doc.data().url, 
-    //                 alt: doc.data().alt
-    //             }))
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const querySnapshot = await getDocs(collection(db, 'slideshow-photos'))
+                const photos = querySnapshot.docs.map(doc => ({
+                    url: doc.data().url, 
+                    alt: doc.data().alt
+                }))
 
-    //             setPhotoArray(photos)
-    //             console.log(photos)
-    //         } catch (error) {
-    //             console.error('Error getting documents: ', error)
-    //         }
-    //     }
+                setPhotoArray(photos)
+                console.log(photos)
+            } catch (error) {
+                console.error('Error getting documents: ', error)
+            }
+        }
 
-    //     fetchData()
-    // }, [])
-
-    setPhotoArray({url: 'src/resources/Seminar - Image 1.jpg', alt: 'Seminar - Image 1'},
-                  {url: 'src/resources/Seminar - Image 2.jpg', alt: 'Seminar - Image 2'},
-                  {url: 'src/resources/Seminar - Image 3.jpg', alt: 'Seminar - Image 3'},
-                  {url: 'src/resources/Speaker - Image 1.jpg', alt: 'Speaker - Image 1'},
-                  {url: 'src/resources/Speaker - Image 2.jpg', alt: 'Speaker - Image 2'})
+        fetchData()
+    }, [])
 
     return (
         <>
